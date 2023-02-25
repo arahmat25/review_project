b0ce17c16c2bb1adb9f91926fdf7f79b899cce77
arahmat25 <90437529+arahmat25@users.noreply.github.com>
2023-02-25 18:06:36 -0500

    Create Gradebook.h

---
+++ Gradebook.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+
+class Gradebook{
+    private:
+    vector<int> labs;
+    vector<int> assignments;
+    unsigned int review_project;
+    unsigned int final_project;
+    unsigned int final_exam;
+    
+    public:
+    Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  unsigned int review_projects_input, unsigned int final_project_input, unsigned int final_exam_input);
+    Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  unsigned int review_projects_input, unsigned int final_project_input);
+    int lab_total();
+    int assignment_total();
+    void individual(string name,int number);
+    void catagory(string name);
+
+    
+};
