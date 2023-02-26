a83520a4ec27a09e242f8fc204bac49999cd0b40
arahmat25 <90437529+arahmat25@users.noreply.github.com>
2023-02-26 17:05:09 -0500

    Update gradebook.cpp

--- gradebook.cpp
+++ gradebook.cpp
@@ -4,22 +4,20 @@
 using namespace std;
 
 //constructor used if final exam is inputed
-Gradebook::Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  unsigned int review_projects_input, unsigned int final_project_input, unsigned int final_exam_input){
+Gradebook::Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  vector<int> projects_input, unsigned int final_exam_input){
     this->labs = labsInput;
     this->assignments = assignmentsInput;
-    this->review_project = review_projects_input;
-    this->final_project = final_project_input;
+    this->projects = projects;
     this->final_exam = final_exam_input;
 
 
 }
 
 //constructor used if final exam is not inputed
-Gradebook::Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  unsigned int review_projects_input, unsigned int final_project_input){
+Gradebook::Gradebook(vector<int> labsInput, vector<int> assignmentsInput, vector<int> projects_input){
     this->labs = labsInput;
     this->assignments = assignmentsInput;
-    this->review_project = review_projects_input;
-    this->final_project = final_project_input;
+    this->projects = projects_input;
     this->final_exam = 0;
  }
 
@@ -38,15 +36,14 @@ int Gradebook::assignment_total(){
     return sum;
 
 }
-
-int Gradebook::course_total(){
-    int cTotal = 0;
-
-    cTotal = lab_total() + assignment_total() + this-> review_project + this-> final_project + this-> final_exam;
-    
-    return cTotal;
-};
-//string has to be Lab or Assignment
+int Gradebook::project_total(){
+    int sum = 0;
+    for(int i = 0; i < this->projects.size(); i++){
+        sum += projects[i];
+    }
+    return sum;
+}
+//string has to be the name of 
 //prints Lab/Assignment and its number plus the grade gotton from 
  void Gradebook::individual(string name,int number){
     if(name == "Lab"){
@@ -56,21 +53,18 @@ int Gradebook::course_total(){
     else if (name == "Assignment"){
         cout << "Assignment" << number << ": " << assignments[number-1] << endl;
     }
-    else if (name == "Review Project"){
-        cout << "Review Project: " << this->review_project << endl; 
-    }
-    else if(name == "Final Project"){
-        cout << "Final Project: " << this->final_project << endl;
+    else if (name == "Projects"){
+        cout << "Project" << number << ": " << projects[number-1] << endl;
     }
     else if (name == "Final Exam"){
         cout << "Final Exam: " << this->final_exam << endl;
     }
  }
- 
+
  void Gradebook::category(string name,string YN){
     //print all lab grades if YN equals No, only lab total when YN equals YES
     if(name == "Lab"){
-        if(YN == "No"){
+        
         for(int i = 0; i < labs.size(); i++){
         cout << "Lab " << (i +1) << ": " << labs[i] << endl;
     }
@@ -78,33 +72,40 @@ int Gradebook::course_total(){
         if(YN == "Yes"){
         cout<< "Lab Total:" << lab_total() << endl;
         }
-    }
     else if (name == "Assignment"){
         //print all lab grades if YN equals No, only lab total when YN equals YES
-        if(YN == "No"){
+        
         for(int i = 0; i < assignments.size(); i++){
         cout << "Assignment " << (i +1) << ": " << assignments[i] << endl;
     }
-        }
         if(YN == "Yes"){
         cout<< "Assignment: " << assignment_total() << endl;
         }
     }
-    else if (name == "Review Project"){
-        cout << "Review Project: " << this->review_project << endl; 
-    }
-    else if(name == "Final Project"){
-        cout << "Final Project: " << this->final_project << endl;
+    else if (name == "Projects"){
+        for(int i = 0; i < projects.size(); i++){
+            cout << "Project " << (i +1) << ": " << assignments[i] << endl;
+        }
+        if(YN == "Yes"){
+          cout<< "Projects: " << project_total() << endl;  
+        }
     }
     else if (name == "Final Exam"){
         cout << "Final Exam: " << this->final_exam << endl;
     }
 
  }
+ int Gradebook::course_total(){
+    int cTotal = 0;
 
- void Gradebook::course(int number){
+    cTotal = lab_total() + assignment_total() + project_total() + this-> final_exam;
+    
+    return cTotal;
+};
+
+void Gradebook::course(int number){
     //all grades within their respective categories and a course overall
-    if(number == 1){
+    if(number = 1){
        category("Lab", "No");
        category("Assignment", "No");
        category("Review Project", "No");
@@ -114,7 +115,7 @@ int Gradebook::course_total(){
        course_total();
     }
     //only category totals and course overall
-    else if(number == 2){
+    else if(number = 2){
        category("Lab", "Yes");
        category("Assignment", "Yes");
        category("Review Project", "Yes");
@@ -123,7 +124,7 @@ int Gradebook::course_total(){
        course_total();
     }
     //only the course overall
-    else if(number == 3){
+    else if(number = 3){
        course_total();
     }
 
