5cd7ab00e8aaa7cd1858344be854886960667504
arahmat25 <90437529+arahmat25@users.noreply.github.com>
2023-02-26 17:05:32 -0500

    Update Gradebook.h

--- Gradebook.h
+++ Gradebook.h
@@ -7,18 +7,18 @@ class Gradebook{
     private:
     vector<int> labs;
     vector<int> assignments;
-    unsigned int review_project;
-    unsigned int final_project;
+    vector<int> projects;
     unsigned int final_exam;
     
     public:
-    Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  unsigned int review_projects_input, unsigned int final_project_input, unsigned int final_exam_input);
-    Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  unsigned int review_projects_input, unsigned int final_project_input);
+    Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  vector<int> projects, unsigned int final_exam_input);
+    Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  vector<int> projects);
     int lab_total();
     int assignment_total();
-    int course_total();
+    int project_total();
     void individual(string name,int number);
-    void catagory(string name);
-    void course(int number);
+    void category(string name,string YN);
+    int course_total();
+    void course(int number);\
     
 };
