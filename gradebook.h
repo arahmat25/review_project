972a43464ca9d766efa4ccb99aaa8a302163ba7c
nwainwright2 <71286374+nwainwright2@users.noreply.github.com>
2023-02-26 18:01:58 -0500

    Add files via upload

---
+++ gradebook.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Gradebook{
+private:
+    vector<int> labs;
+    vector<int> assignments;
+    vector<int> projects;
+    unsigned int final_exam;
+
+public:
+    Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  vector<int> projects, unsigned int final_exam_input);
+    Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  vector<int> projects);
+    int lab_total();
+    int assignment_total();
+    int project_total();
+    int course_total();
+    void course(int number);
+    void individual(string name,int number);
+    void category(string name, string YN);
+
+};
