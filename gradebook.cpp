1166cf65704fdd58d156d2ea796c111ca09e9f00
Max Krusz <98424311+maxKrusz@users.noreply.github.com>
2023-02-26 17:40:33 -0500

    Add files via upload
    minor tweaks to code tidiness. added and changed various comments

--- gradebook.cpp
+++ gradebook.cpp
@@ -1,26 +1,25 @@
-#include <iostream> 
+#include <iostream>
 #include "gradebook.h"
-#include <vector> 
+#include <vector>
 using namespace std;
 
-//constructor used if final exam is inputed
-Gradebook::Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  vector<int> projects_input, unsigned int final_exam_input){
+// Constructor used to generate if there is final exam input
+Gradebook::Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  vector<int> projectsInput, unsigned int final_exam_input){
     this->labs = labsInput;
     this->assignments = assignmentsInput;
-    this->projects = projects;
+    this->projects = projectsInput;
     this->final_exam = final_exam_input;
-
-
 }
 
-//constructor used if final exam is not inputed
-Gradebook::Gradebook(vector<int> labsInput, vector<int> assignmentsInput, vector<int> projects_input){
+// Constructor used to generate if there is no final exam input
+Gradebook::Gradebook(vector<int> labsInput, vector<int> assignmentsInput, vector<int> projectsInput){
     this->labs = labsInput;
     this->assignments = assignmentsInput;
-    this->projects = projects_input;
+    this->projects = projectsInput;
     this->final_exam = 0;
- }
+}
 
+// Returns total of all lab grades
 int Gradebook::lab_total(){
     int sum = 0;
     for(int i = 0; i < labs.size(); i++){
@@ -28,6 +27,8 @@ int Gradebook::lab_total(){
     }
     return sum;
 }
+
+// Returns total of all assignment grades
 int Gradebook::assignment_total(){
     int sum = 0;
     for(int i = 0; i < assignments.size(); i++){
@@ -36,6 +37,8 @@ int Gradebook::assignment_total(){
     return sum;
 
 }
+
+// Returns total of both project grades
 int Gradebook::project_total(){
     int sum = 0;
     for(int i = 0; i < this->projects.size(); i++){
@@ -43,91 +46,93 @@ int Gradebook::project_total(){
     }
     return sum;
 }
-//string has to be the name of 
-//prints Lab/Assignment and its number plus the grade gotton from 
- void Gradebook::individual(string name,int number){
+
+// Returns an individual grade from any of the categories
+void Gradebook::individual(string name,int number){
+    // Returns an individual lab grade
     if(name == "Lab"){
         cout << "Lab " << number << ": " << labs[number-1] << endl;
-
     }
+    // Returns an individual assignment grade
     else if (name == "Assignment"){
         cout << "Assignment" << number << ": " << assignments[number-1] << endl;
     }
+    // Returns an individual project grade
     else if (name == "Projects"){
         cout << "Project" << number << ": " << projects[number-1] << endl;
     }
+    // Returns an final exam grade
     else if (name == "Final Exam"){
         cout << "Final Exam: " << this->final_exam << endl;
     }
- }
+}
 
- void Gradebook::category(string name,string YN){
+// Returns a category's individual grades and has the option of returning the category total
+void Gradebook::category(string name,string YN){
     //print all lab grades if YN equals No, only lab total when YN equals YES
     if(name == "Lab"){
-        
         for(int i = 0; i < labs.size(); i++){
-        cout << "Lab " << (i +1) << ": " << labs[i] << endl;
-    }
+            cout << "Lab " << (i +1) << ": " << labs[i] << endl;
         }
         if(YN == "Yes"){
-        cout<< "Lab Total:" << lab_total() << endl;
+            cout<< "Lab Total: " << lab_total() << endl;
         }
+    }
+
     else if (name == "Assignment"){
         //print all lab grades if YN equals No, only lab total when YN equals YES
-        
         for(int i = 0; i < assignments.size(); i++){
-        cout << "Assignment " << (i +1) << ": " << assignments[i] << endl;
-    }
+            cout << "Assignment " << (i +1) << ": " << assignments[i] << endl;
+        }
         if(YN == "Yes"){
-        cout<< "Assignment: " << assignment_total() << endl;
+            cout<< "Assignment: " << assignment_total() << endl;
         }
     }
+
     else if (name == "Projects"){
         for(int i = 0; i < projects.size(); i++){
             cout << "Project " << (i +1) << ": " << assignments[i] << endl;
         }
         if(YN == "Yes"){
-          cout<< "Projects: " << project_total() << endl;  
+            cout<< "Projects: " << project_total() << endl;
         }
     }
+
     else if (name == "Final Exam"){
         cout << "Final Exam: " << this->final_exam << endl;
     }
 
- }
- int Gradebook::course_total(){
-    int cTotal = 0;
+}
 
+// Returns total grade for entire course - adds all totals together and final
+int Gradebook::course_total(){
+    int cTotal = 0;
     cTotal = lab_total() + assignment_total() + project_total() + this-> final_exam;
-    
     return cTotal;
 };
 
+// Returns three different outputs, the user chooses based on an integer selection of 1, 2, or 3.
 void Gradebook::course(int number){
-    //all grades within their respective categories and a course overall
-    if(number = 1){
-       category("Lab", "No");
-       category("Assignment", "No");
-       category("Review Project", "No");
-       category("Final Project", "No");
-       category("Final Exam", "No");
+    // Returns every grade in each category and course total - does NOT return category totals
+    if(number == 1){
+        category("Lab", "No");
+        category("Assignment", "No");
+        category("Review Project", "No");
+        category("Final Project", "No");
+        category("Final Exam", "No");
         //Course Overall
-       course_total();
+        course_total();
     }
-    //only category totals and course overall
-    else if(number = 2){
-       category("Lab", "Yes");
-       category("Assignment", "Yes");
-       category("Review Project", "Yes");
-       category("Final Project", "Yes");
-       category("Final Exam", "Yes");
-       course_total();
+        // Returns only category and course totals
+    else if(number == 2){
+        cout << assignment_total() << endl;
+        cout << lab_total() << endl;
+        cout << project_total() << endl;
+        course_total();
     }
-    //only the course overall
-    else if(number = 3){
-       course_total();
+        // Returns only course total
+    else if(number == 3){
+        course_total();
     }
 
- }
-
- 
+}
