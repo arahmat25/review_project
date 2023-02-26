2497f1cc8bdc333cb60b43aef18aee496eaaa49f
EasChen70 <90417966+EasChen70@users.noreply.github.com>
2023-02-25 19:09:07 -0500

    Update Gradebook.h

--- Gradebook.h
+++ Gradebook.h
@@ -16,8 +16,9 @@ class Gradebook{
     Gradebook(vector<int> labsInput, vector<int> assignmentsInput,  unsigned int review_projects_input, unsigned int final_project_input);
     int lab_total();
     int assignment_total();
+    int course_total();
     void individual(string name,int number);
     void catagory(string name);
-
+    void course(int number);
     
 };
